a58e17a05c8a7ee757abde278976b18d976e6d8f
yang_tian <2493095695@qq.com>
2025-06-03 09:31:10 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     open: true
   },
   // /bansou.github.io
-  base: '/bansou.github.io',
+  base: './',
   build: {
     outDir: 'dist', //输出目录
     // assetsDir: 'assets', //静态资源目录
