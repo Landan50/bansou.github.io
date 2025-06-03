3c5900cf7aaab4fb56b0fde15df15c5133bcf34f
yang_tian <2493095695@qq.com>
2025-06-03 09:07:07 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -83,8 +83,8 @@ const routes = [
 ]
 
 const router = createRouter({
-    // history: createWebHashHistory(),
-    history: createWebHistory(),
+    history: createWebHashHistory(),
+    // history: createWebHistory(),
     routes,
     scrollBehavior(to, from, scrollBehavior) {
         return new Promise((resolve, reject) => {
